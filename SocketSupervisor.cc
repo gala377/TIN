8079c7f8a8fcb58b3fd4ef7d45acecca5486bf8a
Jakub Gajownik <gajus123@gmail.com>
2018-04-21 14:46:01 +0200

    Initial commit

---
+++ SocketSupervisor.cc
@@ -0,0 +1,5 @@
+//
+// Created by gajus123 on 21.04.18.
+//
+
+#include "SocketSupervisor.h"
---
+++ SocketSupervisor.h
@@ -0,0 +1,14 @@
+//
+// Created by gajus123 on 21.04.18.
+//
+
+#ifndef TIN_SOCKETSUPERVISOR_H
+#define TIN_SOCKETSUPERVISOR_H
+
+
+class SocketSupervisor {
+
+};
+
+
+#endif //TIN_SOCKETSUPERVISOR_H
