297755f151d14675debdc26021f6e2d5268af2f9
Jakub Gajownik <gajus123@gmail.com>
2018-04-21 14:56:02 +0200

    Added private members

--- SocketSupervisor.h
+++ SocketSupervisor.h
@@ -5,6 +5,7 @@
 #ifndef TIN_SOCKETSUPERVISOR_H
 #define TIN_SOCKETSUPERVISOR_H
 
+#include <unordered_map>
 #include "TCPServer.h"
 #include "TCPSocket.h"
 
@@ -14,6 +15,11 @@ public:
     void add(TCPServer* server);
     void remove(TCPSocket* socket);
     void remove(TCPServer* server);
+private:
+    int pipe_input_;
+    int pipe_output_;
+    std::unordered_map<int, TCPSocket*> sockets_;
+    std::unordered_map<int, TCPServer*> servers_;
 };
 
 
