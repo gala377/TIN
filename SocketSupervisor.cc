0b24ed09c90a306b4478181bcf125d58fde0ab17
Jakub Gajownik <gajus123@gmail.com>
2018-04-21 15:06:30 +0200

    Added pipe creating/closing

--- SocketSupervisor.cc
+++ SocketSupervisor.cc
@@ -3,3 +3,36 @@
 //
 
 #include "SocketSupervisor.h"
+
+
+SocketSupervisor::SocketSupervisor() {
+    int descriptors[2];
+    pipe(descriptors);
+    pipe_input_ = descriptors[1];
+    pipe_output_ = descriptors[0];
+}
+
+SocketSupervisor::~SocketSupervisor() {
+    close(pipe_input_);
+    close(pipe_output_);
+}
+
+void SocketSupervisor::add(TCPSocket* socket) {
+    //TODO
+}
+
+void SocketSupervisor::add(TCPServer* server) {
+    //TODO
+}
+
+void SocketSupervisor::remove(TCPSocket* socket) {
+    //TODO
+}
+
+void SocketSupervisor::remove(TCPServer* server) {
+    //TODO
+}
+
+void SocketSupervisor::run() {
+    //TODO
+}
