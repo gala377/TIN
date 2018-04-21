11e81de50c87a573af4bb5fefcbb3484a73cd22c
Jakub Gajownik <gajus123@gmail.com>
2018-04-21 14:47:56 +0200

    Added public interface

--- SocketSupervisor.h
+++ SocketSupervisor.h
@@ -5,9 +5,15 @@
 #ifndef TIN_SOCKETSUPERVISOR_H
 #define TIN_SOCKETSUPERVISOR_H
 
+#include "TCPServer.h"
+#include "TCPSocket.h"
 
 class SocketSupervisor {
-
+public:
+    void add(TCPSocket* socket);
+    void add(TCPServer* server);
+    void remove(TCPSocket* socket);
+    void remove(TCPServer* server);
 };
 
 
