67f79f7d9ca24156e68c9f09d04f85994f0c9c65
Jakub Gajownik <gajus123@gmail.com>
2018-04-25 17:15:03 +0200

    Finished basic implementation

--- SocketUnix.cc
+++ SocketUnix.cc
@@ -3,3 +3,31 @@
 //
 
 #include "SocketUnix.h"
+
+int SocketUnix::select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout) {
+    return ::select(nfds, readfds, writefds, exceptfds, timeout);
+}
+
+int SocketUnix::accept(int s, struct sockaddr * restrict_addr, socklen_t * restrict_addrlen) {
+    return ::accept(s, restrict_addr, restrict_addrlen);
+}
+
+int SocketUnix::connect(int s, const struct sockaddr *name, socklen_t namelen) {
+    return ::connect(s, name, namelen);
+}
+
+int SocketUnix::socket(int domain, int type, int protocol) {
+    return ::socket(domain, type, protocol);
+}
+
+int SocketUnix::listen(int s, int backlog) {
+    return ::listen(s, backlog);
+}
+
+int SocketUnix::close(int fd) {
+    return ::close(fd);
+}
+
+int SocketUnix::getErrno() {
+    return errno;
+}
--- SocketUnix.h
+++ SocketUnix.h
@@ -5,9 +5,17 @@
 #ifndef TIN_SOCKETUNIX_H
 #define TIN_SOCKETUNIX_H
 
+#include "SocketFacade.h"
 
 class SocketUnix {
-
+public:
+    virtual int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
+    virtual int accept(int s, struct sockaddr * restrict_addr, socklen_t * restrict_addrlen);
+    virtual int connect(int s, const struct sockaddr *name, socklen_t namelen);
+    virtual int socket(int domain, int type, int protocol);
+    virtual int listen(int s, int backlog);
+    virtual int close(int fd);
+    virtual int getErrno();
 };
 
 
