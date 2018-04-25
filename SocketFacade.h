aeba59b4cf265e21bfca65110190475158dc092d
Jakub Gajownik <gajus123@gmail.com>
2018-04-25 17:07:45 +0200

    Basic socket facade

--- SocketFacade.h
+++ SocketFacade.h
@@ -5,9 +5,25 @@
 #ifndef TIN_SOCKETFACADE_H
 #define TIN_SOCKETFACADE_H
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <memory.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
 
 class SocketFacade {
-
+public:
+    virtual int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout) = 0;
+    virtual int accept(int s, struct sockaddr * restrict_addr, socklen_t * restrict_addrlen) = 0;
+    virtual int connect(int s, const struct sockaddr *name, socklen_t namelen) = 0;
+    virtual int socket(int domain, int type, int protocol) = 0;
+    virtual int listen(int s, int backlog) = 0;
+    virtual int close(int fd) = 0;
+    virtual int errno() = 0;
 };
 
 
