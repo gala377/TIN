3a703daa817cf686f8988b974763ecc171fe00af
Jakub Gajownik <gajus123@gmail.com>
2018-04-26 10:35:10 +0200

    Empty class

---
+++ TCPSocketWithPacket.cc
@@ -0,0 +1,5 @@
+//
+// Created by gajus123 on 26.04.18.
+//
+
+#include "TCPSocketWithPacket.h"
---
+++ TCPSocketWithPacket.h
@@ -0,0 +1,14 @@
+//
+// Created by gajus123 on 26.04.18.
+//
+
+#ifndef TIN_TCPSOCKETWITHPACKET_H
+#define TIN_TCPSOCKETWITHPACKET_H
+
+
+class TCPSocketWithPacket {
+
+};
+
+
+#endif //TIN_TCPSOCKETWITHPACKET_H
