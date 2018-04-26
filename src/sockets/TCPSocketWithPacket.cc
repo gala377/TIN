cfa6144b13136e236f2a767788c17759b3840656
Jakub Gajownik <gajus123@gmail.com>
2018-04-26 10:51:03 +0200

    Finished basic implementation

--- src/sockets/TCPSocketWithPacket.cc
+++ src/sockets/TCPSocketWithPacket.cc
@@ -2,4 +2,25 @@
 // Created by gajus123 on 26.04.18.
 //
 
-#include "TCPSocketWithPacket.h"
+#include "../../inc/sockets/TCPSocketWithPacket.h"
+
+TCPSocketWithPacket::TCPSocketWithPacket(SocketFacade* socket_interface) :
+    TCPSocket(socket_interface) {
+}
+
+TCPSocketWithPacket::TCPSocketWithPacket(SocketFacade* socket_interface, int socket, SocketState state) :
+    TCPSocket(socket_interface, socket, state) {
+
+}
+
+TCPSocketWithPacket::TCPSocketWithPacket(TCPSocketWithPacket&& other) :
+    TCPSocket(std::move(other)) {
+}
+
+TCPSocketWithPacket& TCPSocketWithPacket::operator=(TCPSocketWithPacket&& other) {
+    TCPSocket::operator=(std::move(other));
+    return *this;
+}
+
+TCPSocketWithPacket::~TCPSocketWithPacket() {
+}
