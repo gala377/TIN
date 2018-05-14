6b41c91cc634311c06a929d4ebe14d9b0e144d07
Jakub Gajownik <gajus123@gmail.com>
2018-05-14 18:00:05 +0200

    Finished Acknowledge implementation

--- inc/messages/Acknowledge.h
+++ inc/messages/Acknowledge.h
@@ -5,4 +5,22 @@
 #ifndef TIN_ACKNOWLEDGE_H
 #define TIN_ACKNOWLEDGE_H
 
+#include "Message.h"
+
+class Acknowledge : public Message {
+public:
+    Acknowledge(int t) : b(t) {}
+
+    int b;
+private:
+    Acknowledge() {}
+    friend class boost::serialization::access;
+
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version) {
+        ar & boost::serialization::base_object<Message>(*this);
+        ar & b;
+    }
+};
+
 #endif //TIN_ACKNOWLEDGE_H
--- src/messages/Acknowledge.cc
+++ src/messages/Acknowledge.cc
@@ -2,3 +2,6 @@
 // Created by gajus123 on 14.05.18.
 //
 
+#include "../../inc/messages/Acknowledge.h"
+
+BOOST_CLASS_EXPORT(Acknowledge)
