f56f414a726a21a9cce6f04e31c77045a3cd5774
Jakub Gajownik <gajus123@gmail.com>
2018-05-14 17:58:59 +0200

    Finished message base class

--- inc/messages/Message.h
+++ inc/messages/Message.h
@@ -5,4 +5,29 @@
 #ifndef TIN_MESSAGE_H
 #define TIN_MESSAGE_H
 
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/serialization/export.hpp>
+
+class Message {
+public:
+    virtual ~Message() {}
+    int a;
+
+    std::string toString();
+    static Message* fromString(const std::string& buffer);
+protected:
+    Message() {};
+private:
+
+    friend class boost::serialization::access;
+
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version) {
+        ar & a;
+    }
+};
+
 #endif //TIN_MESSAGE_H
--- src/messages/Message.cc
+++ src/messages/Message.cc
@@ -2,3 +2,25 @@
 // Created by gajus123 on 14.05.18.
 //
 
+#include "../../inc/messages/Message.h"
+
+std::string Message::toString() {
+    std::string result;
+    boost::iostreams::back_insert_device<std::string> inserter(result);
+    boost::iostreams::stream<boost::iostreams::back_insert_device<std::string> > s(inserter);
+    boost::archive::binary_oarchive outputArchive(s);
+
+    outputArchive << &(*this);
+    s.flush();
+    return result;
+}
+
+Message* Message::fromString(const std::string& buffer) {
+    Message* result;
+    boost::iostreams::basic_array_source<char> device(buffer.data(), buffer.size());
+    boost::iostreams::stream<boost::iostreams::basic_array_source<char> > a(device);
+    boost::archive::binary_iarchive inputArchive(a);
+
+    inputArchive >> result;
+    return result;
+}
