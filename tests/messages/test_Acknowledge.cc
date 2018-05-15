29cd5504b9179c372e0c6b0a8a003d9b795b3357
Jakub Gajownik <gajus123@gmail.com>
2018-05-15 11:18:46 +0200

    Added basic tests

--- tests/messages/test_Acknowledge.cc
+++ tests/messages/test_Acknowledge.cc
@@ -2,3 +2,29 @@
 // Created by gajus123 on 15.05.18.
 //
 
+#include <gtest/gtest.h>
+
+#include "../../inc/messages/Acknowledge.h"
+
+TEST(AcknowledgeTests, SerializationEquality) {
+    Acknowledge ack(1242);
+    Message* mess = &ack;
+    std::string temp = mess->toString();
+    Message* mess2 = Message::fromString(temp);
+    Acknowledge* ack2 = dynamic_cast<Acknowledge*>(mess2);
+    ASSERT_EQ(ack2 != nullptr, true);
+    EXPECT_EQ(ack.id_, ack2->id_);
+}
+
+TEST(AcknowledgeTests, SerializationWithBuffer) {
+    Acknowledge ack(1242);
+    Message* mess = &ack;
+    std::string temp = mess->toString();
+    boost::asio::streambuf buffer;
+    std::ostream os(&buffer);
+    os << temp;
+    Message* mess2 = Message::fromBuffer(buffer);
+    Acknowledge* ack2 = dynamic_cast<Acknowledge*>(mess2);
+    ASSERT_EQ(ack2 != nullptr, true);
+    EXPECT_EQ(ack.id_, ack2->id_);
+}
--- tests/tests_main.cc
+++ tests/tests_main.cc
@@ -2,3 +2,9 @@
 // Created by gajus123 on 15.05.18.
 //
 
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
