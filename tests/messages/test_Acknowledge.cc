de24887111de246a4c39a9733255736c65495c5e
Jakub Gajownik <gajus123@gmail.com>
2018-05-21 22:08:08 +0200

    Changed from pointer to shared_ptr

--- tests/messages/test_Acknowledge.cc
+++ tests/messages/test_Acknowledge.cc
@@ -10,8 +10,8 @@ TEST(AcknowledgeTests, SerializationEquality) {
     Acknowledge ack(1242);
     Message* mess = &ack;
     std::string temp = mess->toString();
-    Message* mess2 = Message::fromString(temp);
-    Acknowledge* ack2 = dynamic_cast<Acknowledge*>(mess2);
+    auto mess2 = Message::fromString(temp);
+    Acknowledge* ack2 = dynamic_cast<Acknowledge*>(mess2.get());
     ASSERT_EQ(ack2 != nullptr, true);
     EXPECT_EQ(ack.id_, ack2->id_);
 }
@@ -23,8 +23,8 @@ TEST(AcknowledgeTests, SerializationWithBuffer) {
     boost::asio::streambuf buffer;
     std::ostream os(&buffer);
     os << temp;
-    Message* mess2 = Message::fromBuffer(buffer);
-    Acknowledge* ack2 = dynamic_cast<Acknowledge*>(mess2);
+    auto mess2 = Message::fromBuffer(buffer);
+    Acknowledge* ack2 = dynamic_cast<Acknowledge*>(mess2.get());
     ASSERT_EQ(ack2 != nullptr, true);
     EXPECT_EQ(ack.id_, ack2->id_);
 }
