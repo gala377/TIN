df2f244c4ee760a24ff74950ee7dcb11c7dc8c79
Jakub Gajownik <gajus123@gmail.com>
2018-05-22 23:13:00 +0200

    Added basic class implementation

--- inc/system/CommunicationModule.h
+++ inc/system/CommunicationModule.h
@@ -5,6 +5,30 @@
 #ifndef TIN_COMMUNICATIONMODULE_H
 #define TIN_COMMUNICATIONMODULE_H
 
+#include <memory>
 
+#include "sockets/TCPServer.h"
+#include "sockets/TCPSocket.h"
+#include "sockets/SocketSupervisor.h"
+#include "sockets/SocketUnix.h"
+
+class CommunicationModule {
+public:
+    CommunicationModule(CommunicationModule& other) = delete;
+    CommunicationModule(CommunicationModule&& other);
+    CommunicationModule& operator=(CommunicationModule& other) = delete;
+    CommunicationModule& operator=(CommunicationModule&& other);
+    ~CommunicationModule();
+
+    static CommunicationModule createServer(uint16_t port);
+    static CommunicationModule createClient(uint16_t port, Sockets::IP address);
+private:
+    CommunicationModule(uint16_t port);
+
+    Sockets::SocketUnix socket_facade_;
+    Sockets::TCPServer server_;
+    std::shared_ptr<Sockets::TCPSocket> socket_;
+    Sockets::SocketSupervisor supervisor_;
+};
 
 #endif //TIN_COMMUNICATIONMODULE_H
--- src/system/CommunicationModule.cc
+++ src/system/CommunicationModule.cc
@@ -1,1 +1,39 @@
 #include "system/CommunicationModule.h"
+
+CommunicationModule::CommunicationModule(uint16_t port) :
+    socket_facade_(),
+    server_(&socket_facade_),
+    supervisor_(&socket_facade_) {
+    server_.listen(port);
+    //TODO setup business logic
+}
+
+CommunicationModule::~CommunicationModule() {
+    server_.close();
+    if(socket_.get())
+        socket_->close();
+}
+
+CommunicationModule CommunicationModule::createServer(uint16_t port) {
+    CommunicationModule result(port);
+    return result;
+}
+
+CommunicationModule CommunicationModule::createClient(uint16_t port, Sockets::IP address) {
+    CommunicationModule result(port);
+    result.socket_ = std::make_shared<Sockets::TCPSocket>(&result.socket_facade_);
+    result.socket_->connect(address, port);
+    return result;
+}
+
+CommunicationModule::CommunicationModule(CommunicationModule&& other) :
+    server_(std::move(other.server_)),
+    socket_(std::move(other.socket_)),
+    supervisor_(std::move(other.supervisor_)) {
+}
+
+CommunicationModule& CommunicationModule::operator=(CommunicationModule&& other) {
+    server_ = std::move(other.server_);
+    socket_ = std::move(other.socket_);
+    supervisor_ = std::move(other.supervisor_);
+}
